199e9a55684bdae59eb5aacadc6248fda22f0d3e
yuturn <brave870307@gmail.com>
2023-09-13 14:20:31 +0800

    close signup page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -174,12 +174,12 @@ export default function Login({ setUser, setAlert }) {
                                     登入
                                 </Button>
                             </Box>
-                            <Box sx={{ py: 2 }}>
+                            {/* <Box sx={{ py: 2 }}>
                                 如果沒有帳號請點選下方按鈕
                                 <Link href='/login/signup' underline="hover">
                                     註冊帳號
                                 </Link>
-                            </Box>
+                            </Box> */}
                         </Container>
                     </Box>
                 </Grid>
