f2f6a4e32d652d422a965d08ea94464e6d3de57c
yuturn <brave870307@gmail.com>
2023-10-25 18:20:12 +0800

    123

--- src/pages/LOG.js
+++ src/pages/LOG.js
@@ -178,7 +178,7 @@ export default function LOG({ token, ...rest }) {
                                 <Box sx={{ mt: 1, ml: 4 }} display="flex" component="form" role="form">
                                     <Box>
                                         <Typography variant="h5" fontWeight="medium" mr={2}>
-                                            員工ID:
+                                            員工ID :
                                         </Typography>
                                     </Box>
                                     <Box>
